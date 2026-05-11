48eaa0032ff9d8363a08858ad2b8ae983545d422
tnbngoc <tnbngoc912@gmail.com>
2026-05-11 20:47:43 +0700

    fix: fix scroll bug

--- next.config.mjs
+++ next.config.mjs
@@ -15,6 +15,16 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'lh3.googleusercontent.com',
       },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '4000',
+      },
+      {
+        protocol: 'http',
+        hostname: '127.0.0.1',
+        port: '4000',
+      },
     ],
   },
 }
