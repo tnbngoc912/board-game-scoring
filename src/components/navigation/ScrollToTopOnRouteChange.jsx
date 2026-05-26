6933fc3360d709337332cd53536d16541060844f
tnbngoc <tnbngoc912@gmail.com>
2026-05-26 19:53:29 +0700

    Revert "fix: refactor code"
    This reverts commit 9241eb26abac39233846b056418396f21398aa8f.

--- src/components/navigation/ScrollToTopOnRouteChange.jsx
+++ src/components/navigation/ScrollToTopOnRouteChange.jsx
@@ -4,25 +4,11 @@ import { useLayoutEffect } from 'react'
 import { usePathname } from 'next/navigation'
 
 export function ScrollToTopOnRouteChange() {
-  const pathname = usePathname()
+  const pathname = usePathname();
 
   useLayoutEffect(() => {
-    const reset = () => {
-      const activeScreen = document.querySelector('.app-shell .screen')
-      if (activeScreen instanceof HTMLElement) {
-        activeScreen.scrollTop = 0
-        activeScreen.scrollLeft = 0
-      } else {
-        window.scrollTo(0, 0)
-        document.documentElement.scrollTop = 0
-        document.body.scrollTop = 0
-      }
-    }
+    window.scroll(0, 0);
+  }, [pathname]);
 
-    reset()
-    const rafId = window.requestAnimationFrame(reset)
-    return () => window.cancelAnimationFrame(rafId)
-  }, [pathname])
-
-  return null
+  return <></>;
 }
